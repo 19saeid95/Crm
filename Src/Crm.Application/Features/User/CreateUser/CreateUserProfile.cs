153afc7b0184509ca09a31cb14799f94ea989120
Saeid Adabi <saeid.adabi1995@outlook.com>
2026-08-16 19:35:12 +0330

    auto mapper creat user

---
+++ Src/Crm.Application/Features/User/CreateUser/CreateUserProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+
+namespace Crm.Application.Features.User.CreateUser;
+
+public sealed class CreateUserProfile : Profile
+{
+    public CreateUserProfile()
+    {
+        CreateMap<CreateUserCommand, Crm.Domain.Entities.User>()
+            .ForMember(
+                x => x.Id,
+                opt => opt.Ignore())
+            .ForMember(
+                x => x.PasswordHash,
+                opt => opt.Ignore())
+            .ForMember(
+                x => x.CreateDate,
+                opt => opt.Ignore())
+            .ForMember(
+                x => x.LastUpdate,
+                opt => opt.Ignore())
+            .ForMember(
+                x => x.IsDeleted,
+                opt => opt.Ignore())
+            .ForMember(
+                x => x.UserRoles,
+                opt => opt.Ignore());
+    }
+}
